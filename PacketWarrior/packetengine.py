bab26c06966bc8457baa526e55ed1e29e5d846a2
Jonathan Loy <jonrloy@gmail.com>
2013-12-07 22:37:28 -0500

    Packet capturing thread writes the packets to a text file

--- PacketWarrior/packetengine.py
+++ PacketWarrior/packetengine.py
@@ -27,7 +27,7 @@ class DecoderThread(Thread):
             raise Exception("Datalink type not supported: " % datalink)
 
         self.pcap = pcapObj
-        self.myfile = open("test.txt", "w", 66)
+        self.myfile = open("temp.txt", "w", 66)
         Thread.__init__(self)
 
     def run(self):
@@ -44,53 +44,49 @@ class DecoderThread(Thread):
         except Exception, e:
             print e
 
-     def stop(self):
-        print "Trying to stop thread "
-        if self.process is not None:
-            self.process.terminate()
-            self.process = None
-
 class PacketEngine():
 
-	def __init__(self):
-		self.selected_device = None
-		self.available_devices = None
-		self.cap = None
+    def __init__(self):
+        self.selected_device = None
+        self.available_devices = None
+        self.cap = None
+        self.thread = None
 
-	def get_available_devices(self):
-		self.available_devices = pcapy.findalldevs()
-		return self.available_devices
+    def get_available_devices(self):
+        self.available_devices = pcapy.findalldevs()
+        return self.available_devices
 
-	def set_device(self, choice):
-		self.selected_device = choice
-		self.set_packet_reader()
+    def set_device(self, choice):
+        self.selected_device = choice
+        self.set_packet_reader()
 
-	def set_packet_reader(self, sniff_timeout = 1000):
-		'''
-		open device
-		# Arguments here are:
-		#   device
-		#   snaplen (maximum number of bytes to capture _per_packet_)
-		#   promiscious mode (1 for true)
-		#   timeout (in milliseconds)
-		'''
-		try:
-			self.cap = pcapy.open_live(self.selected_device, ETHERNET_MAX_FRAME_SIZE, PROMISC_MODE, sniff_timeout)
-			print "Set to capture on %s: net=%s, mask=%s" % (self.selected_device, self.cap.getnet(), self.cap.getmask())
-		except Exception, e:
-			print "Failed to set packet capture reader : open_live() failed for device='%s'. Error: %s" % (self.selected_device, str(e))
+    def set_packet_reader(self, sniff_timeout = 1000):
+        '''
+        open device
+        # Arguments here are:
+        #   device
+        #   snaplen (maximum number of bytes to capture _per_packet_)
+        #   promiscious mode (1 for true)
+        #   timeout (in milliseconds)
+        '''
+        try:
+            self.cap = pcapy.open_live(self.selected_device, ETHERNET_MAX_FRAME_SIZE, PROMISC_MODE, sniff_timeout)
+            print "Set to capture on %s: net=%s, mask=%s" % (self.selected_device, self.cap.getnet(), self.cap.getmask())
+        except Exception, e:
+            print "Failed to set packet capture reader : open_live() failed for device='%s'. Error: %s" % (self.selected_device, str(e))
 
-	def set_filter(self, filter):
-		self.cap.setfilter(filter)
+    def set_filter(self, filter):
+        self.cap.setfilter(filter)
 
-	def start_capture(self):
-		# Start sniffing thread and finish main thread.
-		self.thread = DecoderThread(self.cap).start()
+    def start_capture(self):
+        # Start sniffing thread and finish main thread.
+        self.thread = DecoderThread(self.cap).start()
 
-	def stop_capture(self):
-		self.thread
-
-	
+    def stop_capture(self):
+        print "Attempting to stop thread"
+        if self.thread is not None:
+            self.thread.terminate()
+            self.thread = None
 
 if __name__ == "__main__":
   p = PacketEngine()
