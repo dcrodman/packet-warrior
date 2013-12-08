3b37c48cfe92c2cb127592cd75c1e4abc8d95053
Jonathan Loy <jonrloy@gmail.com>
2013-12-08 12:00:05 -0500

    altered the filter box values to fit pcapy's syntax

--- PacketWarrior/boxes.py
+++ PacketWarrior/boxes.py
@@ -16,14 +16,15 @@ class FilterBox(tk.Toplevel):
     def combo(self):
     	top = tk.Toplevel()
     	top.geometry('320x100+319+252')
-    	filterLabel = tk.Label(top, text="Select a filter using the up/down arrowhead keys \nor keyboard up/down arrow keys.\n Replace the value in quotes.")
+    	filterLabel = tk.Label(top, text='Select a filter using the up/down arrowhead keys' \
+            '\nor keyboard up/down arrow keys.\n Replace the value in quotes.')
         filterLabel.grid(column=0, row=0)
     	self.box_value = StringVar()
         self.box = ttk.Combobox(top, textvariable=self.box_value)
-        self.box['values'] = ('host \'host\'', 'src host \'host\'', 'dst host \'host\'',
-         'gateway \'host\'', 'dst net \'net\'', 'src net \'net\'', 'net \'net\'', 
-         'dst port \'port\'', 'src port \'port\'', 'port \'port\'', 'ip proto \'protocol\'', 
-         'ip6 proto \'protocol\'','ip broadcast', 'ip multicast', 'ip6 multicast')
+        self.box['values'] = ('tcp', 'udp', 'imcp', 'host \'host\'', 'src host \'host\'', 
+            'dst host \'host\'','gateway \'host\'', 'dst net \'net\'', 'src net \'net\'', 
+            'net \'net\'', 'dst port \'port\'', 'src port \'port\'', 'port \'port\'', 
+            'ip6 proto \'protocol\'','ip broadcast', 'ip multicast', 'ip6 multicast')
         self.box.current(0)
         self.box.grid(column=0, row=1)
 
@@ -35,7 +36,8 @@ class DeviceBox(tk.Toplevel):
     def combo(self, devices):
         top = tk.Toplevel()
         top.geometry('320x100+319+132')
-        filterLabel = tk.Label(top, text="Select a filter using the up/down arrowhead keys \nor keyboard up/down arrow keys.\n Replace the value in quotes.")
+        filterLabel = tk.Label(top, text='Select a filter using the up/down arrowhead keys' \
+            '\nor keyboard up/down arrow keys.\n Replace the value in quotes.')
         filterLabel.grid(column=0, row=0)
         self.box_value = StringVar()
         self.box = ttk.Combobox(top, textvariable=self.box_value)
