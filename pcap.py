98113bc2addab70a9b3f062c3959eae6279f9fd8
Drew Rodman <dcrodman@gmail.com>
2013-12-08 21:18:47 -0500

    Update Python extension test.
    !!!Note!!! This will hang at getNextPacket for some reason, which
    I am currently working on.

--- pcap.py
+++ pcap.py
@@ -1,3 +1,18 @@
-import libpcap_ext
-engine = libpcap_ext.PacketEngine()
-print engine.getAvailableDevices()
+import pcap_ext
+
+engine = pcap_ext.PacketEngine()
+
+
+devices = engine.getAvailableDevices()
+assert len(devices) > 0, "Devices list is empty"
+
+for device in devices:
+	print 'Device: %s' % device
+selected_dev = input("Enter number of device to sniff: ")
+
+engine.setFilter("tcp port 80")
+engine.selectDevice(devices[selected_dev])
+engine.startCapture()
+
+print engine.getNextPacket()
+print "Done"
